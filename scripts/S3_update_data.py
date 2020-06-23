d2d43dce58cdd31ee99406248f4f4dd3c9a8d226
TrailheadsUP EC2 <christine.mf.miller@gmail.com>
2020-06-23 21:21:19 +0000

    data update scipts

--- scripts/S3_update_data.py
+++ scripts/S3_update_data.py
@@ -25,8 +25,8 @@ s3client = boto3.client('s3',
 response = s3client.get_object(Bucket = bucket, Key = data_file)
 body = response["Body"].read()
 previous = pd.read_csv(io.BytesIO(body))
-#print(previous.info())
-#print(previous.tail())
+print(previous.info())
+print(previous.tail())
 
 # find most recent cam pictures on EC2
 base_path = "visionary/data/raw/"
@@ -37,16 +37,18 @@ for (dirpath, dirnames, filenames) in os.walk(base_path):
     break
 
 cam_paths = [base_path + cam + "/*" for cam in cam_list]
+print(cam_paths)
 
 most_recent_files = [ccf.latest_image(path) for path in cam_paths]
 filtered = list(filter(None, most_recent_files))
-#print(filtered)
+print(filtered)
 
 # run newest images through car counting model
 new_counts = [ccf.yolo_counts_dict(image) for image in filtered] # time limiting step
 
 # make new counts into a dataframe
 new_df = pd.DataFrame(new_counts)
+print(new_df.tail(10))
 
 # format datetime for new images
 date_format ='%d-%m-%Y-%H-%M'
@@ -57,8 +59,8 @@ new_df = new_df.drop(['file_name'], 1)
 all_data = pd.concat([previous, new_df])
 all_data = all_data.dropna(axis=0, how= 'any')
 all_data.drop_duplicates(inplace = True)
-#print(all_data.info())
-#print(all_data.tail())
+print(all_data.info())
+print(all_data.tail())
 
 # over write datafile in S3
 s3_resource = boto3.resource('s3')
