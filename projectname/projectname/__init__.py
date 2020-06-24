2c4fc23f6ab9085bfa2aa3e34641c14fce69f435
Christine Miller <christine.mf.miller@gmail.com>
2020-06-24 09:35:15 -0700

    clean up repository

--- projectname/projectname/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 27 21:18:10 2020
-
-@author: cmfmiller
-"""
-
-
--- projectname/projectname/custom_funcs.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 27 21:20:29 2020
-
-@author: cmfmiller
-"""
-
-
